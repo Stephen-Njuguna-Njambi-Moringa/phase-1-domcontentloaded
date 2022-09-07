d9da221c27bc130677e483a14658a5eb9806099d
Stephen Njuguna Njambi <stepehen.jambi@student.moringaschool.com>
2022-09-07 16:36:07 +0300

    lab passed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code goes here
+// Your code goes here.
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelector('p').textContent = 'This is really cool!';
+  });
+
+
+// function update(text){
+//     let p = document.getElementById('text');
+//     p.innerText = text;
+// }
+// update('This is really cool!');
+
